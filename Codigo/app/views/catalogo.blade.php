2626a11b874d98bb881f0711b88d681ef6c5b394
Roberto Damián Venero <roberto.venero@gmail.com>
2014-06-06 03:06:11 -0300

    Cambios estéticos para el catálogo

--- Codigo/app/views/catalogo.blade.php
+++ Codigo/app/views/catalogo.blade.php
@@ -21,11 +21,11 @@ menuActivo='catalogo'
 <a name="area"></a>
 @if(count($libros)> 0)
 
-<h1><font color="purple">Bienvenido a nuestro catálogo </font> </h1>
-<h2>En nuestro catálogo tenemos los siguientes libros para ofrecerle</h2>
+<h1><font color="purple"><center>¡Bienvenido a nuestro catálogo!</center></font> </h1>
+<center><h2>Disponemos de los siguientes libros para ofrecerle:</h2></center>
 @if (! Auth::check())
 
-  </br><b><center><a href="/login" title="Iniciar sesión">Inicie sesión</a> para poder acceder a los detalles de cada libro</b></center>
+  </br><b><center><a href="/login" title="Iniciar sesión"><u>Inicie sesión</u></a> para acceder a los detalles de cada libro</b></center>
 @endif
 <br></br>
 
@@ -36,14 +36,19 @@ menuActivo='catalogo'
                            
                     <h3>{{$libros[($i*4)+$j]->título}} <!--({{$i}} : {{$j}} )--></h3>
                     <img src="/datos/tapas/{{$libros[($i*4)+$j]->tapa}}" alt="Tapa del libro" title="Tapa del libro" width="100" style="float:left ;box-shadow:10px 8px 10px #ccc; border:1px solid #ccc" />
-                    &nbsp;&nbsp;Autor/es:&nbsp;{{implode(', ',array_pluck($libros[($i*4)+$j]->autores,'nombre'))}}<br><br/>
+                    &nbsp;&nbsp;@if(count($libros[$i]->autores)>1)
+                                  Autores:
+                                  @else
+                                  Autor:
+                                  @endif
+                                  {{implode(', ',array_pluck($libros[($i*4)+$j]->autores,'nombre'))}}<br><br/>
                     &nbsp;&nbsp;Editorial:&nbsp;{{$libros[($i*4)+$j]->editorial->nombre}}<br><br/>
-                    &nbsp;&nbsp;Precio: $ {{$libros[($i*4)+$j]->precio}}<br><br/>
+                    &nbsp;&nbsp;Precio: ${{$libros[($i*4)+$j]->precio}}<br><br/>
                     @if ($libros[($i*4)+$j]->agotado != 0)
-                       <b><u><font color="purple"> Agotado en este momento </font></u></b>  <br></br>
+                       <strong><font size=4px color="red">Agotado</font></strong>  <br></br>
                     @endif
                     @if (! Auth::guest())
-                    <a href="/{{$libros[($i*4)+$j]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center" >Ver mas</a><br><br/> 
+                    <a href="/{{$libros[($i*4)+$j]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center">Ver detalles</a><br><br/> 
                     @endif
                 </div>
             </div>
@@ -57,14 +62,19 @@ menuActivo='catalogo'
                   <!--<b><u><font color="red"> Agotado en este momento </font></u></b>   -->  
                     <h3>{{$libros[$i+(floor(count($libros)/4)*4)]->título}} </h3>
                     <img src="/datos/tapas/{{$libros[$i+(floor(count($libros)/4)*4)]->tapa}}" alt="Tapa del libro" title="Tapa del libro" width="100" style="float:left ;box-shadow:10px 8px 10px #ccc; border:1px solid #ccc" />
-                    &nbsp;&nbsp;Autor/es:&nbsp;{{implode(', ',array_pluck($libros[$i+(floor(count($libros)/4)*4)]->autores,'nombre'))}}<br><br/>
+                    &nbsp;&nbsp;@if(count($libros[$i]->autores)>1)
+                                  Autores:
+                                  @else
+                                  Autor:
+                                  @endif
+                                  {{implode(', ',array_pluck($libros[$i+(floor(count($libros)/4)*4)]->autores,'nombre'))}}<br><br/>
                     &nbsp;&nbsp;Editorial:&nbsp;{{$libros[$i+(floor(count($libros)/4)*4)]->editorial->nombre}}<br><br/>
-                    &nbsp;&nbsp;Precio: $ {{$libros[$i+(floor(count($libros)/4)*4)]->precio}}<br><br/>
+                    &nbsp;&nbsp;Precio: ${{$libros[$i+(floor(count($libros)/4)*4)]->precio}}<br><br/>
                     @if ($libros[$i+(floor(count($libros)/4)*4)]->agotado != 0)
-                       <b><u><font color="cookbook"> Agotado en este momento </font></u></b>  <br></br>
+                       <strong><font size=4px color="red">Agotado</font></strong>  <br></br>
                     @endif
                     @if (! Auth::guest())
-                    <a href="/{{$libros[$i+(floor(count($libros)/4)*4)]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center" >Ver mas</a><br><br/> 
+                    <a href="/{{$libros[$i+(floor(count($libros)/4)*4)]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center">Ver detalles</a><br><br/> 
                     @endif
                 </div>
             </div>            
@@ -77,14 +87,19 @@ menuActivo='catalogo'
                   <!--<b><u><font color="red"> Agotado en este momento </font></u></b>   -->  
                     <h3>{{$libros[$i]->título}} </h3>
                     <img src="/datos/tapas/{{$libros[$i]->tapa}}" alt="Tapa del libro" title="Tapa del libro" width="100" style="float:left ;box-shadow:10px 8px 10px #ccc; border:1px solid #ccc" />
-                    &nbsp;&nbsp;Autor/es:&nbsp;{{implode(', ',array_pluck($libros[$i]->autores,'nombre'))}}<br><br/>
+                    &nbsp;&nbsp;@if(count($libros[$i]->autores)>1)
+                                  Autores:
+                                  @else
+                                  Autor:
+                                  @endif
+                                  {{implode(', ',array_pluck($libros[$i]->autores,'nombre'))}}<br><br/>
                     &nbsp;&nbsp;Editorial:&nbsp;{{$libros[$i]->editorial->nombre}}<br><br/>
-                    &nbsp;&nbsp;Precio: $ {{$libros[$i]->precio}}<br><br/>
+                    &nbsp;&nbsp;Precio: ${{$libros[$i]->precio}}<br><br/>
                     @if ($libros[$i]->agotado != 0)
-                       <b><u><font color="cookbook"> Agotado en este momento </font></u></b>  <br></br>
+                       <strong><font size=4px color="red">Agotado</font></strong>  <br></br>
                     @endif
                     @if (! Auth::guest())
-                    <a href="/{{$libros[$i]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center" >Ver mas</a><br><br/> 
+                    <a href="/{{$libros[$i]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center">Ver detalles</a><br><br/> 
                     @endif
                 </div>
             </div>            
@@ -94,20 +109,25 @@ menuActivo='catalogo'
                   <!--<b><u><font color="red"> Agotado en este momento </font></u></b>   -->  
                     <h3>{{$libros[$i]->título}} </h3>
                     <img src="/datos/tapas/{{$libros[$i]->tapa}}" alt="Tapa del libro" title="Tapa del libro" width="100" style="float:left ;box-shadow:10px 8px 10px #ccc; border:1px solid #ccc" />
-                    &nbsp;&nbsp;Autor/es:&nbsp;{{implode(', ',array_pluck($libros[$i]->autores,'nombre'))}}<br><br/>
+                    &nbsp;&nbsp; @if(count($libros[$i]->autores)>1)
+                                  Autores:
+                                  @else
+                                  Autor:
+                                  @endif
+                                  {{implode(', ',array_pluck($libros[$i]->autores,'nombre'))}}<br><br/>
                     &nbsp;&nbsp;Editorial:&nbsp;{{$libros[$i]->editorial->nombre}}<br><br/>
-                    &nbsp;&nbsp;Precio: $ {{$libros[$i]->precio}}<br><br/>
+                    &nbsp;&nbsp;Precio: ${{$libros[$i]->precio}}<br><br/>
                     @if ($libros[$i]->agotado != 0)
-                       <b><u><font color="cookbook"> Agotado en este momento </font></u></b>  <br></br>
+                       <strong><font size=4px color="red">Agotado</font></strong>   <br></br>
                     @endif
                     @if (! Auth::guest())
-                    <a href="/{{$libros[$i]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center" >Ver mas</a><br><br/> 
+                    <a href="/{{$libros[$i]->id}}/detalles" title="Vea los detalles, solo si inicio sesión" class="button button-mediano" style="float:center">Ver detalles</a><br><br/> 
                     @endif
                 </div>
           @endif
        <br class="separador" /><br></br>  
       @endif  
 @else
-     <h1><font color="purple">En este momento no tenemos libros para mostrar, sepa disculparnos. </font> </h1>
+     <h1><font color="purple">En este momento, no disponemos de libros para ofrecerle. Disculpe las molestias.</font></h1>
 @endif   
 @stop
