cdf373c0a486de5021538848b37981707301c232
Roberto Damián Venero <roberto.venero@gmail.com>
2014-06-14 00:38:37 -0300

    Fix para el dropdown de Provincia

--- Codigo/app/views/usuario/perfil.blade.php
+++ Codigo/app/views/usuario/perfil.blade.php
@@ -16,25 +16,70 @@
 @endif
 
 <form method="post" action="/perfil">
-	Nombre: <input name="nombre" value="{{Input::old('nombre',Auth::user()->nombre)}}"/><span class="tooltip" title="El nombre debe tener una longitud mayor a 2 caracteres.">[?]</span><br/>
-	Apellido: <input name="apellido" value="{{Input::old('apellido',Auth::user()->apellido)}}"/><span class="tooltip" title="El apellido debe tener una longitud mayor a 2 caracteres.">[?]</span><br/>
-	Email: <input name="email" value="{{Input::old('email',Auth::user()->email)}}"/><span class="tooltip" title="El email debe ser una dirección de correo válida.">[?]</span><br/>
-	DNI: <input name="dni" value="{{Input::old('dni',Auth::user()->dni)}}"/><span class="tooltip" title="El DNI tener 7 u 8 dígitos y debe estar separado por puntos.">[?]</span><br/>
-	Teléfono: <input name="teléfono" value="{{Input::old('teléfono',Auth::user()->teléfono)}}"/> <span class="tooltip" title="El teléfono tener un mínimo de 7 caracteres. Se admiten caracteres especiales (paréntesis, -, #, +, etc).">[?]</span> <br/>
-	Provincia:<span class="tooltip" title="Seleccione una provincia de la lista.">[?]</span> <select name="provincia">              
-                @foreach($provincias as $provincia)
-                	@if($provincia->id == Auth::user()->provincia->id) {
-                    {{'<option value="'. $provincia->id .'" selected="Auth::user()->provincia" >'. $provincia->nombre .'</option>'}};
-                    }
-                    @else{
-                    {{'<option value="'. $provincia->id .'">'. $provincia->nombre .'</option>'}};
-                    }
-                    @endif
-                @endforeach</select> 
-    Localidad: <input name="localidad" value="{{Input::old('localidad',Auth::user()->localidad)}}"/> <span class="tooltip" title="Escriba su localidad en 5 o más caracteres.">[?]</span> <br/>
-    Domicilio: <input name="dirección" value="{{Input::old('dirección',Auth::user()->dirección)}}"/> <span class="tooltip" title="Escriba su domicilio en 7 o más caracteres.">[?]</span> <br/>
-	Contraseña: <input type=password name="contraseña" value=""/><span class="tooltip" title="La contraseña debe tener una longitud mayor a 5 caracteres.">[?]</span><br/>
-	Reescriba la contraseña: <input type=password name="contraseña_confirmation" value=""/><span class="tooltip" title="Repita la contraseña para confirmar su modificación.">[?]</span> <br/>
+	<table>
+	<tr>
+		<td>
+		Nombre: <input name="nombre" value="{{Input::old('nombre',Auth::user()->nombre)}}"/><span class="tooltip" title="El nombre debe tener una longitud mayor a 2 caracteres.">[?]</span>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Apellido: <input name="apellido" value="{{Input::old('apellido',Auth::user()->apellido)}}"/><span class="tooltip" title="El apellido debe tener una longitud mayor a 2 caracteres.">[?]</span>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Email: <input name="email" value="{{Input::old('email',Auth::user()->email)}}"/><span class="tooltip" title="El email debe ser una dirección de correo válida.">[?]</span>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		DNI: <input name="dni" value="{{Input::old('dni',Auth::user()->dni)}}"/><span class="tooltip" title="El DNI tener 7 u 8 dígitos y debe estar separado por puntos.">[?]</span>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Teléfono: <input name="teléfono" value="{{Input::old('teléfono',Auth::user()->teléfono)}}"/> <span class="tooltip" title="El teléfono tener un mínimo de 7 caracteres. Se admiten caracteres especiales (paréntesis, -, #, +, etc).">[?]</span> 
+		</td>
+	</tr>
+	<tr>
+		<td style="width:50%">
+		Provincia:
+		</td>
+		<td>
+		<select style="width:55%; padding: 0.50em 0.5em; margin: 0.50em 0.5em; right:80%" name="provincia"> 
+	                @foreach($provincias as $provincia)
+	                	@if($provincia->id == Auth::user()->provincia->id) {
+	                    {{'<option value="'. $provincia->id .'" selected="Auth::user()->provincia" >'. $provincia->nombre .'</option>'}};
+	                    }
+	                    @else{
+	                    {{'<option value="'. $provincia->id .'">'. $provincia->nombre .'</option>'}};
+	                    }
+	                    @endif
+	                @endforeach</select>
+	    </td> 
+	</tr>
+	<tr>
+		<td>
+   		Localidad: <input name="localidad" value="{{Input::old('localidad',Auth::user()->localidad)}}"/> <span class="tooltip" title="Escriba su localidad en 5 o más caracteres.">[?]</span>
+   		</td> 
+    </tr>
+    <tr>
+    	<td>
+   		Domicilio: <input name="dirección" value="{{Input::old('dirección',Auth::user()->dirección)}}"/> <span class="tooltip" title="Escriba su domicilio en 7 o más caracteres.">[?]</span>
+   		</td> 
+    </tr>
+    <tr>
+    	<td>
+		Contraseña: <input type=password name="contraseña" value=""/><span class="tooltip" title="La contraseña debe tener una longitud mayor a 5 caracteres.">[?]</span>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Reescriba la contraseña: <input type=password name="contraseña_confirmation" value=""/><span class="tooltip" title="Repita la contraseña para confirmar su modificación.">[?]</span> <br/>
+		</td>
+	</tr>
+	</table>
 	<br/>
 	<input type="submit" value="Modificar" title="Modificar los datos" />		
 	<a href="/admin/usuarios/" style="text-decoration:none;">
